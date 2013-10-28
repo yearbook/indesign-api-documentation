2a4345eda20ec5d36272ffa974b4a7d0853b4f46
Will Ridgers <will@mindfuzz.net>
2013-10-28 23:57:28 +0000

    Cleaned repo after accidental push. Added README.md

--- docs/js/controllers/ClassDetailCtrl.js
+++
@@ -1,6 +0,0 @@
-function ClassDetailCtrl($scope, $routeParams, $http) {
-  // XHR  to get class details
-  $http.get('./source/' + $routeParams.namespace + '/classes/' + $routeParams.className + '.json').success(function(data) {
-    $scope.class = data;
-  });
-}
--- docs/js/controllers/SearchCtrl.js
+++
@@ -1,6 +0,0 @@
-function SearchCtrl($scope, $http) {
-  // XHR to get the map data
-  $http.get('./source/search.json').success(function(data) {
-    $scope.search = data;
-  });
-}
